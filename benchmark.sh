632b685936ae3078ab61fa6d0b25170e68c51cff
mdeboute <martin.deboute@gmail.com>
2022-04-21 23:24:47 +0200

    refactoring

--- benchmark.sh
+++ benchmark.sh
@@ -6,10 +6,10 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
-echo Experimental Campaign:
-echo Data directory: $1
-echo Output directory: $2
-echo Model: $3
+echo "Experimental Campaign:"
+echo "Data directory: $1"
+echo "Output directory: $2"
+echo "Model: $3"
 
 mkdir -p $2
 cd $2 && mkdir -p $3
@@ -18,8 +18,14 @@ cd ../../
 
 for instance in `ls $1`; do
     echo Resolution of $instance
-    python3 src/$3.py $1/$instance $2/$3/
+    log="log_${instance%.dat}.txt"
+    python3 src/$3.py $1/$instance > $2/$3/$log
 done
 
-grep Result $2/$3/*.sol >> $2/$3/results.csv
+cd $2
+grep Result $3/*.txt > $3/results.csv
+# for each line of the results.csv file remove all before ':'
+sed 's/^.*://' $3/results.csv > $3/results.csv
+
+echo "Experimental Campaign finished!"
 exit 0
--- sol/test_correle/test_500_1000_r0.01-0.sol
+++
@@ -1,4 +0,0 @@
-Resolution status: OPTIMAL
-Resolution time (sec) : 0.192
-----------------------------------
-Result; test_500_1000_r0.01-0; 0.192; OptimizationStatus.OPTIMAL; 1850.0

35b3e96dc0f397b8edbe6b4e3c7cca5d1c9998c7
mdeboute <martin.deboute@gmail.com>
2022-05-06 23:11:24 +0200

    refactor

--- src/relax_disj.py
+++ src/relax_disj.py
@@ -16,23 +16,15 @@ def calc_disj_expression(graph, v, x):
 def relax_disj(graph, c, v):
     n = graph.get_n()
     vertex = graph.get_vertices_info()
-
     model = Model(name="relax_disj", solver_name="CBC")
     model.verbose = 0
-
     x = [model.add_var(name="x_%s" % i, var_type=BINARY) for i in range(n)]
-
     disj_expression = calc_disj_expression(graph, v, x)
-
     model.objective = minimize(
         xsum(-vertex[i].get_profit() * x[i] for i in range(n)) + disj_expression
     )
-
     model.add_constr(xsum(vertex[i].get_weight() * x[i] for i in range(n)) <= c)
-
-    #model.optimize(max_seconds=2)
     model.optimize()
-
     return x, model
 
 
@@ -53,9 +45,12 @@ def indexesSortedByProfit(vertex):
     return J
 
 
-# Fonction glouton pour trouver une solution réalisable pour le problème d'origine
-# qui servira de borne supérieure au problème.
 def upper_bound(graph, c):
+    """
+    Greedy function to find a viable solution to the original problem
+    which will serve as the upper bound for the problem.
+    """
+
     n = graph.get_n()
     vertex = graph.get_vertices_info()
     I = indexesSortedByProfit(vertex)
@@ -80,11 +75,11 @@ def sub_gradient(graph, x):
     for i in range(n):
         for j in range(n):
             if i != j and graph.get_adj_matrix()[i][j] == 1:
-                s.append((x[i].x + x[j].x - 1)**2)
+                s.append((x[i].x + x[j].x - 1))
     return sum(s)
 
 
-def find_v(graph, c, alpha=1e-2, epsilon=1e-2):
+def find_v(graph, c, alpha=1, epsilon=1e-2):
     """
     It's a subgradient algorithm to find the best v coefficient
     to solve the Lagrangian dual problem of the Disjunctive relax KP MILP.
@@ -97,39 +92,37 @@ def find_v(graph, c, alpha=1e-2, epsilon=1e-2):
 
     x, model = relax_disj(graph, c, v)
     subGradient = [sub_gradient(graph, x)]
+
     if subGradient[-1] == 0:
         return v
+
     has_improved = True
     while has_improved:
-        s = alpha * (ub - model.objective_value) / subGradient[-1]
+        s = alpha * (ub - model.objective_value) / (abs(subGradient[-1]) ** 2)
         has_improved = False
         for i in range(n):
             for j in range(n):
                 if i != j and graph.get_adj_matrix()[i][j] == 1:
-                    #s = 0
-                    #if ((x[i].x + x[j].x - 1)!=0):
-                    #    s = alpha * (ub - model.objective_value) / ((x[i].x + x[j].x - 1) ** 2)
-
                     if abs(v_prec[i][j] - v[i][j]) > epsilon:
-                        
-                        #print(v_prec[i][j] , v[i][j], end="   ")
                         v_prec[i][j] = v[i][j]
                         v[i][j] = max(0, v[i][j] + s * (x[i].x + x[j].x - 1))
                         if abs(v_prec[i][j] - v[i][j]) > epsilon:
-                            print(s, "   ", (x[i].x + x[j].x - 1), "   " , v_prec[i][j] , v[i][j])
+                            print(
+                                "s: ",
+                                s,
+                                "   ",
+                                "Ax - b: ",
+                                (x[i].x + x[j].x - 1),
+                                "   ",
+                                "v_prec/v: ",
+                                v_prec[i][j],
+                                v[i][j],
+                            )
                             has_improved = True
-
         x, model = relax_disj(graph, c, v)
-        if (model.status == OptimizationStatus.INFEASIBLE):
-            for i in range(n):
-                for j in range(n):
-                    if i != j and graph.get_adj_matrix()[i][j] == 1:
-                        if v[i][j]!=0:
-                            print(v[i][j])
         subGradient.append(sub_gradient(graph, x))
         if subGradient[-1] == 0:
             break
-
     return v
 
 
@@ -147,12 +140,10 @@ if __name__ == "__main__":
     end = time.time()
 
     if v is not None:
-        print("Result: v = %s" % v)
         print("Time: %s" % (end - start))
-
         x, model = relax_disj(graph, c, v)
         if model.status == OptimizationStatus.OPTIMAL:
-            print("Relaxed model objective value:", model.objective_value)
+            print("Relaxed model objective value:", -model.objective_value)
             vertex = graph.get_vertices_info()
             profit = xsum(vertex[i].get_profit() * x[i] for i in range(n)).x
             weight = xsum(vertex[i].get_weight() * x[i] for i in range(n)).x

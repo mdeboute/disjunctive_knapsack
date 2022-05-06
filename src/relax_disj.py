984d4f0d0f4696a1b74aacde418e58233a24127e
mdeboute <martin.deboute@gmail.com>
2022-05-06 17:21:10 +0200

    gl lin

--- src/relax_disj.py
+++ src/relax_disj.py
@@ -1,6 +1,6 @@
 from utils import parser
 from mip import *
-import sys, time
+import sys, time, math
 
 
 def calc_disj_expression(graph, v, x):
@@ -9,7 +9,7 @@ def calc_disj_expression(graph, v, x):
     for i in range(n):
         for j in range(n):
             if i != j and graph.get_adj_matrix()[i][j] == 1:
-                disj_expression += v[i][j] * (x[i] + x[j] - 1)
+                disj_expression += v[i] * (1 - x[i] - x[j])
     return disj_expression
 
 
@@ -35,35 +35,49 @@ def relax_disj(graph, c, v):
     return x, model
 
 
-def find_v(graph, c, v, alpha=1, epsilon=1e-3):
+def sub_gradient(graph, x):
+    n = graph.get_n()
+    s = []
+    for i in range(n):
+        for j in range(n):
+            if i != j and graph.get_adj_matrix()[i][j] == 1:
+                s.append(1 - x[i].x - x[j].x)
+    return sum(s)
+
+
+def L(x, v):
+    # TODO: l(v) = cx + v(b-Ax)
+    return
+
+
+def find_v(graph, c, alpha=1, epsilon=1e-3):
     """
-    It's a subgradient algorithm to find the best phi coefficient
+    It's a subgradient algorithm to find the best v coefficient
     to solve the Lagrangian dual problem of the Disjunctive relax KP MILP.
     """
 
+    n = graph.get_n()
+    v = [0 for _ in range(n)]
     x, model = relax_disj(graph, c, v)
-    thetas = [model.objective_value]
-
-    if model.status == OptimizationStatus.OPTIMAL:
+    ub = [model.objective_value]
+    subGradient = [sub_gradient(graph, x)]
+    if subGradient[-1] == 0:
         return v
+    while True:
+        s = (alpha * (ub[-1] - L(x, v))) / (abs(subGradient[-1]) ** 2)
+        v = (
+            v + s * subGradient[-1]
+        )  # TODO: find a way to multipply a vector by a scalar in python
+        print(v)
+        x, model = relax_disj(graph, c, v)
+        ub.append(model.objective_value)
+        subGradient.append(sub_gradient(graph, x))
+        if abs(ub[-1] - ub[-2]) < epsilon:
+            break
+        if subGradient[-1] == 0:
+            break
 
-    phi = v
-
-    switch = True
-    while switch == True:
-        ub = thetas[-1]
-        disj_expression = calc_disj_expression(graph, v, x)
-        s = alpha * (ub - thetas[-1]) / (abs(disj_expression) ** 2)
-        phi = phi + s * disj_expression
-        x, model = relax_disj(graph, c, v=phi)
-        thetas.append(model.objective_value)
-        if model.status == OptimizationStatus.OPTIMAL:
-            switch = False
-        if abs(thetas[-1] - thetas[-2]) < epsilon:
-            switch = False
-        print(phi)
-
-    return phi
+    return subGradient
 
 
 if __name__ == "__main__":
@@ -72,17 +86,7 @@ if __name__ == "__main__":
         exit(1)
 
     fileName = sys.argv[1]
-
     graph, c = parser(fileName)
-    n = graph.get_n()
-    v = [[0 for i in range(n)] for j in range(n)]
 
-    start = time.time()
-    v = find_v(graph, c, v)
-    end = time.time()
-
-    if v is not None:
-        print("Result: %s" % v)
-        print("Time: %s" % (end - start))
-    else:
-        print("No coeff found!")
+    v = find_v(graph, c)
+    print(v)
